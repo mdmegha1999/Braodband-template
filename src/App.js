a093af500255a98e07daa228fe1b18779b96c59f
Megha Digarse <94183187+mdmegha1999@users.noreply.github.com>
2024-02-04 18:16:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,156 @@
+import "./App.css";
+import { Phone, Mail } from "react-feather";
+
+function App() {
+  return (
+    <div className="App container">
+      <header className="App-header">
+        <div className="shop-name-container">
+          <h1 className="shop-name">M A LINE</h1>
+          <h4 className="service-type">BROADBAND SERCICES</h4>
+        </div>
+        <div className="logo-container">
+          <img src="/Airtel_Xstream_logo.png" alt="brand_image" />
+        </div>
+      </header>
+      <main className="App-main">
+        <div className="plans-container">
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+          <div className="plan-card">
+            <div className="plan-price">Rs. 499</div>
+            <div className="plan-name">Xstream Fiber Tariff</div>
+            <ul className="plan-benefits">
+              <li>Unlimited Data</li>
+              <li>40Mbps speed</li>
+              <li>Free voice calling</li>
+              <li>Airtel Xstream, Wynk Music, and Shaw Academy subscription</li>
+            </ul>
+          </div>
+        </div>
+      </main>
+      <footer className="App-footer">
+        <div className="address-container">
+          <div className="footer-label">Address</div>
+          <a
+            href="https://goo.gl/maps/9B3DsTEzBi47ra2s8"
+            rel="noreferrer"
+            target="_blank"
+            className="footer-info"
+          >
+          At. Post Rondha, District-Betul, Madhya Pradesh 460001
+          </a>
+        </div>
+        <div className="contact-container">
+          <div className="footer-label">Contact</div>
+          <a
+            href="tel:+91999999999"
+            rel="noreferrer"
+            target="_blank"
+            className="footer-info"
+          >
+            <Phone /> +91 9999 99999
+          </a>
+          <a
+            href="https://wa.me/91999999999"
+            rel="noreferrer"
+            target="_blank"
+            className="footer-info"
+          >
+            <Phone /> +91 9999 99999
+          </a>
+          <a
+            href="mailto:email@example.com"
+            rel="noreferrer"
+            target="_blank"
+            className="footer-info"
+          >
+            <Mail /> email@example.com
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+// Customer ko apun ka text msg karna hai attatime sab ko jana ohh msg
+// contact ko apun ke offer ke bare mai msg karna
